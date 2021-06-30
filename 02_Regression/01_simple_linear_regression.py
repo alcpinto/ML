b867c9e9dc8ffbc4dc3132c251a8dad8bd12c95d
Abilio Pinto <abiliop@intenda.uk.com>
2021-06-30 17:42:04 +0100

    Simple Linear Regression final model

--- 02_Regression/01_simple_linear_regression.py
+++ 02_Regression/01_simple_linear_regression.py
@@ -14,9 +14,41 @@ print(y)
 
 # Splitting the dataset into Traning set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 print(X_train)
 print(y_train)
 print(X_test)
 print(y_test)
 
+# Traning the Simple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+print(y_pred)
+
+# Visualising the Training set result
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs. Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs. Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Making a single prediction (employee with 12 years of experience)
+print(regressor.predict([[12]]))
+
+# Getting the final linear regression equation with the values of the coefficients
+print(regressor.intercept_)
+print(regressor.coef_)
+# Salary = 26780.10 + 9312.58 * YearsExperience
