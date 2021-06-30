77579a0ef2cc19c39446454acbd462704ac5d2fc
Abilio Pinto <abiliop@intenda.uk.com>
2021-06-30 16:39:06 +0100

    Created folder to hold the files containing data

--- 01_Data_Preprocessing/data_preprocessing.py
+++ 01_Data_Preprocessing/data_preprocessing.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('/home/abilio/repos/training/MachineLearning/ML/01_Data_Preprocessing/Data.csv')
+dataset = pd.read_csv('/home/abilio/repos/training/MachineLearning/ML/01_Data_Preprocessing/data/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
