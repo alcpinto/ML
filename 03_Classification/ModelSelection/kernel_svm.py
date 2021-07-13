0722805a91b63b21e9cc285e68b9125fc89cfc23
Abilio Pinto <abiliop@intenda.uk.com>
2021-07-13 14:47:08 +0100

    Demo model selection

--- 03_Classification/ModelSelection/kernel_svm.py
+++ 03_Classification/ModelSelection/kernel_svm.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
